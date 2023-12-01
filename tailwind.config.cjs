4b191cd81e210a7702383d341ae9480e899b852b
gshah2020 <gaurang.r.shah@gmail.com>
2023-12-01 16:55:47 -0500

    update theme colors usage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,5 +19,61 @@ module.exports = {
 			},
 		},
 	},
-	 plugins: [require('@tailwindcss/typography')],
+	 plugins: [require('@tailwindcss/typography'), require("daisyui")],
+	 daisyui: {
+		/** @link: https://daisyui.com/docs/config/ */
+		themes: [{
+			'gs-light': {
+				'primary': 'rgb(133, 133, 204)',
+				'primary-focus': 'rgb(117, 117, 183)',
+				'primary-content': 'rgb(216, 216, 243)',
+				'secondary': 'rgb(247, 240, 217)',
+				'secondary-focus': 'rgb(244, 241, 210)',
+				'secondary-content': 'rgb(247, 251, 251)',
+				'accent': 'rgb(117, 213, 108)',
+				'accent-focus': 'rgb(106, 211, 93)',
+				'accent-content': 'rgb(217, 247, 217)',
+				'neutral': 'rgb(255, 255, 255)',
+				'neutral-focus': 'rgb(255, 255, 255)',
+				'neutral-content': 'rgb(0, 0, 0)',
+				// 'base-100': '#ffffff',
+				// 'base-200': '#ffffff',
+				// 'base-300': '#ffffff',
+				'base-content': 'rgb(0, 0, 0)',
+				'info': 'rgb(64, 149, 255)',
+				'success': 'rgb(0, 255, 128)',
+				'warning': 'rgb(255, 179, 64)',
+				'error': 'rgb(255, 107, 107)',
+			},
+			'gs-dark': {
+				'primary': 'rgb(7, 3, 64)',
+				'primary-focus': 'rgb(255, 255, 255)',
+				'primary-content': 'rgb(54, 54, 114)',
+				'secondary': 'rgb(227, 250, 219)',
+				'secondary-focus': 'rgb(255, 255, 255)',
+				'secondary-content': 'rgb(177, 177, 177)',
+				'accent': 'rgb(28, 206, 49)',
+				'accent-focus': 'rgb(0, 128, 0)',
+				'accent-content': 'rgb(13, 57, 27)',
+				'neutral': 'rgb(255, 255, 255)',
+				'neutral-focus': 'rgb(255, 255, 255)',
+				'neutral-content': 'rgb(0, 0, 0)',
+				// 'base-100': '#ffffff',
+				// 'base-200': '#ffffff',
+				// 'base-300': '#ffffff',
+				'base-content': 'rgb(0, 0, 0)',
+				'info': 'rgb(0, 64, 255)',
+				'success': 'rgb(34, 131, 27)',
+				'warning': 'rgb(255, 128, 0)',
+				'error': 'rgb(163, 15, 15)',
+			},
+		}], // false: only light + dark | true: all themes or use ['dark', 'light'] to specify specific themes
+		darkTheme: 'gs-dark',
+ 		base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+	 }
 }
