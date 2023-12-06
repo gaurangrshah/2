127a7cffa0d6361fac9ea03056774409ddc6ff38
gshah2020 <gaurang.r.shah@gmail.com>
2023-12-05 23:11:30 -0500

    create send-email api route

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,35 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 import { defineConfig } from 'astro/config';
 // import icon from 'astro-icon';
 
 import tailwind from "@astrojs/tailwind";
-import { analytics, site } from "./src/config.json"
-
+import { analytics, site } from "./src/config.json";
+import vercel from "@astrojs/vercel/serverless";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+// https://astro.build/config
 export default defineConfig({
-  site: site.site_url, // @TODO: replace URL
+  site: site.site_url,
+  // @TODO: replace URL
   base: site.base_path,
   trailingSlash: site.trailingSlash ? 'always' : 'never',
-  output: 'static',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false, // allows base styles to be overridden
-    }),
-    // icon({
-    //   include: {
-    //   },
-    // }),
+  output: 'hybrid',
+  integrations: [tailwind({
+    applyBaseStyles: false // allows base styles to be overridden
+  })
+  // icon({
+  //   include: {
+  //   },
+  // }),
   ],
-   vite: {
+
+  vite: {
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, './src'),
-      },
-    },
+        '~': path.resolve(__dirname, './src')
+      }
+    }
   },
+  adapter: vercel()
 });
---
+++ src/pages/api/send-email.ts
@@ -0,0 +1,49 @@
+import SendEmail from '../../lib/nodemailer';
+
+import type { APIRoute } from 'astro';
+
+
+export const POST: APIRoute = async ({request}) => {
+  const body = await request.json();
+
+  if(body) {
+    const name = body.name;
+    const email = body.email;
+    const message = body.message;
+    if (!name || !email || !message) {
+      return new Response(
+        JSON.stringify({
+          message: "Missing required fields",
+        }),
+        { status: 400 }
+      );
+    }
+
+    const sendEmail = new SendEmail();
+
+    const emailOptions = {
+      from: `"${name}" <${email}>`,
+      to: import.meta.env.SMTP_USER,
+      subject: 'Your email subject',
+      text: message || 'This is a plain text email.',
+      html: message || 'This is an HTML email.',
+    };
+
+    sendEmail.sendEmail(emailOptions);
+
+    return new Response(
+      JSON.stringify({
+        message: "Success!"
+      }),
+      { status: 200 }
+    );
+  } else {
+    return new Response(
+      JSON.stringify({
+        message: "No fields provided.",
+      }),
+      { status: 400 }
+    );
+  }
+
+};
